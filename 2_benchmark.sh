551c95dd466fed14b8f513f60dee70e7b54ea70c
tahara <tahara@userlocal.jp>
2019-05-30 10:36:39 +0900

    fix: どのディレクトリからでも

--- 2_benchmark.sh
+++ 2_benchmark.sh
@@ -1,1 +1,1 @@
-docker run -v /home/tahara/docker:/works -it --rm --runtime=nvidia tahara/keras-sample python works/cifar10_cnn.py
+docker run -v `pwd`:/works -it --rm --runtime=nvidia tahara/keras-sample python works/cifar10_cnn.py
