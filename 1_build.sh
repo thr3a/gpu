ebacff7452472f4c43f38cf2d303fd03d134bb36
tahara <tahara@userlocal.jp>
2019-06-06 17:25:58 +0900

    clean: ディレクトリ化した

--- 1_build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t tahara/keras-sample --no-cache .
--- init_cifar10.py
+++
@@ -1,5 +0,0 @@
-from keras.datasets import cifar10
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
